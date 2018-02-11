49ed822563913546af109fe92c03d069d6a5d80a
dbondar <kdfstudio@gmail.com>
2018-02-11 01:01:12 -0500

    updates

--- cython_evaluation/fastest_evaluate.c
+++ cython_evaluation/fastest_evaluate.c
@@ -2,6 +2,8 @@
 
 int eval(double* out, int size_out, int x_num, double x_min, double x_max, double h_min, double h_max)
 {
+    const double dx = (x_max - x_min) / (x_num - 1);
+
     #pragma omp parallel for
     for(int h_i = 0; h_i < size_out; h_i++){
 
@@ -9,20 +11,10 @@ int eval(double* out, int size_out, int x_num, double x_min, double x_max, doubl
 
         double result = 0.;
 
-        for(int x_i = 0; x_i < x_num; x_i++){
-            const double X = x_min + (x_max - x_min) * x_i / (x_num - 1);
-
-            for(int y_i = 0; y_i < x_num; y_i++){
-                const double Y = x_min + (x_max - x_min) * y_i / (x_num - 1);
-
-                for (int z_i = 0; z_i < x_num; z_i++) {
-                    const double Z = x_min + (x_max - x_min) * z_i / (x_num - 1);
-
+        for(double X = x_min; X < x_max + dx; X += dx)
+            for(double Y = x_min; Y < x_max + dx; Y += dx)
+                for (double Z = x_min; Z < x_max + dx; Z += dx)
                     result += exp(-pow(2. * Z + X + pow(Y, 2) - h, 2)) * (sin(X + Y + 3. * Z + h) + pow(Y + Z + h, 2));
-                }
-            }
-
-        }
 
         out[h_i] = result;
     }
@@ -30,4 +22,3 @@ int eval(double* out, int size_out, int x_num, double x_min, double x_max, doubl
     // success
     return 0;
 }
-
